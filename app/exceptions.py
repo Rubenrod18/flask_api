ba308058379b7942d86d7b8affe3093833590151
Rubén Rodríguez <rubenrod.018@gmail.com>
2020-12-28 00:08:03 +0100

    refactor(exceptions): correct error handler to marshmallow validation error class

--- app/exceptions.py
+++ app/exceptions.py
@@ -7,18 +7,15 @@ flask-restx: https://flask-restx.readthedocs.io/en/latest/errors.html
 """
 import traceback
 
-from flask_restx import Api
-from flask import current_app
+from flask import current_app, Flask
 from marshmallow import ValidationError
 
 
-def init_app(app) -> None:
+def init_app(app: Flask) -> None:
     app.errorhandler(ValidationError)(handle_validation_error_exception)
 
 
-def handle_validation_error_exception(ex):
+def handle_validation_error_exception(ex: ValidationError) -> tuple:
     if current_app.debug:
         traceback.print_exc()
-
-    delattr(ex, 'data')  # Must to be deleted for returning our custom message.
     return {'message': ex.messages}, 422
