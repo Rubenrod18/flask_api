8ebc7ae8d3a59c407441e16c1929229710615a51
Rubén Rodríguez <rubenrod.018@gmail.com>
2021-01-12 19:19:54 +0100

    tests(middleware): add new test for checking no api routes

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,9 +1,14 @@
 from tests.custom_flask_client import CustomFlaskClient
 
 
-def test_middleware(client: CustomFlaskClient, auth_header: any):
+def test_api_middleware(client: CustomFlaskClient, auth_header: any):
     response = client.post('/api/auth/logout', headers=auth_header())
     json_response = response.get_json()
 
     assert 400 == response.status_code
     assert 'Content type no valid' == json_response.get('message')
+
+
+def test_no_api_middleware(client: CustomFlaskClient):
+    response = client.post('/welcome')
+    assert 404 == response.status_code
