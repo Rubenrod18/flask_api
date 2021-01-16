ccbfcdab4ffa9b26fa26bf3ada06f4b09739741b
Rubén Rodríguez <rubenrod.018@gmail.com>
2021-01-16 11:46:23 +0100

    refactor(swagger): update order field format

--- app/models/user.py
+++ app/models/user.py
@@ -38,7 +38,6 @@ class User(BaseModel, UserMixin):
         return super(User, self).save(*args, **kwargs)
 
     def get_reset_token(self) -> str:
-        # TODO: move to user's service
         secret_key = current_app.config.get('SECRET_KEY')
         expire_in = current_app.config.get('RESET_TOKEN_EXPIRES')
         salt = expire_in.__str__()
@@ -51,7 +50,6 @@ class User(BaseModel, UserMixin):
 
     @staticmethod
     def verify_reset_token(token: str) -> any:
-        # TODO: move to user's service
         secret_key = current_app.config.get('SECRET_KEY')
         expire_in = current_app.config.get('RESET_TOKEN_EXPIRES')
         salt = expire_in.__str__()
@@ -68,10 +66,4 @@ class User(BaseModel, UserMixin):
 
     @staticmethod
     def ensure_password(plain_text: str) -> str:
-        # TODO: move to user'service
-        hashed_password = None
-
-        if plain_text:
-            hashed_password = hash_password(plain_text)
-
-        return hashed_password
+        return hash_password(plain_text) if plain_text else None
