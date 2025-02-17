00f893b83602fb04d99a10520a3dfef22c3a6535
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-02-17 12:23:12 +0100

    test: add field_operator eq with str value

---
+++ src/tests/utils/unit/test_request_query_operator.py
@@ -0,0 +1,20 @@
+from app.database.factories.user_factory import UserFactory
+from app.extensions import db
+from app.models import User
+from app.utils.request_query_operator import RequestQueryOperator as rqo
+from tests.base.base_test import TestBase
+
+
+class TestCreateSearchQueryStrings(TestBase):
+    def setUp(self):
+        super(TestCreateSearchQueryStrings, self).setUp()
+        # TODO: pending to define tests of ne, contains, ncontains, startswith, endswith
+
+    def test_create_search_query_str(self):
+        user = UserFactory()
+        kwargs = {'search': [{'field_name': 'name', 'field_operator': 'eq', 'field_value': user.name}]}
+
+        query = db.session.query(User)
+        query = rqo.create_search_query(User, query, kwargs)
+
+        self.assertEqual(query.all(), 1)
