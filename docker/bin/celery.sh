ddef07c74c78523462faf28b9b84700730158f91
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-02-18 18:04:49 +0100

    build(docker): add files to init the application

---
+++ docker/bin/celery.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+mkdir -p /var/run/celeryd
+mkdir -p /var/log/celeryd
+
+celery multi start ${CELERYD_NODES} \
+    --workdir=${HOMEDIR} \
+    --pidfile=/var/run/celeryd/%n.pid \
+    --logfile=/var/log/celeryd/%n%I.log \
+    --loglevel=DEBUG \
+    --app=app.celery \
+    ${CELERYD_OPTS//\"}
+
+
+
+tail -F /var/log/celeryd/*.log
---
+++ docker/bin/gunicorn.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+# This causes the script to exit immediately if any command returns a non-zero exit status (an error).
+# It helps prevent the script from continuing after encountering an error.
+
+set -o pipefail
+# Normally, in a pipeline (e.g, command1 | command2), only the exit status of the last command (command2) is considered.
+# With pipefail, the pipeline will return the exit status of the first command that fails, making it easier to catch
+# errors in complex pipelines.
+
+set -o nounset
+# This makes the script treat the use of unset variables as an error.
+# If the script tries to use an undefined variable, it will immediately exit with an error, preventing subtle bugs.
+
+# TODO: Temporal solution. I need to find a better way to run the gunicorn
+gunicorn --config /usr/local/etc/gunicorn.py
