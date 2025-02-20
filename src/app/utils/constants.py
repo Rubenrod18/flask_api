7f6fc39c8b068619a7ca483a598d45964c12bafb
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-02-20 13:31:19 +0100

    chore: remove some constants

--- src/app/utils/constants.py
+++ src/app/utils/constants.py
@@ -1,10 +1,3 @@
-# Regex
-BIRTH_DATE_REGEX = r'^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$'
-EMAIL_REGEX = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-
-# Auth
-TOKEN_REGEX = r'^Bearer\s(\S+)$'
-
 # Files
 PDF_MIME_TYPE = 'application/pdf'
 MS_WORD_MIME_TYPE = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
