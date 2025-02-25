2ec5ed6761395cc8f839beb35361905b2c9b98b7
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-02-25 12:36:04 +0100

    refactor(roles): move serializers to blueprint

--- src/app/services/role.py
+++ src/app/services/role.py
@@ -1,35 +1,27 @@
 from app.extensions import db
 from app.managers import RoleManager
 from app.models import Role, User, user_datastore
-from app.serializers import RoleSerializer, SearchSerializer
 from app.services.base import BaseService
 
 
 class RoleService(BaseService):
     def __init__(self):
-        super().__init__(manager=RoleManager(), serializer=RoleSerializer(), search_serializer=SearchSerializer())
+        super().__init__(manager=RoleManager())
 
     def create(self, **kwargs) -> Role:
-        data = self.serializer.load(kwargs)
-        return self.manager.create(**data)
+        return self.manager.create(**kwargs)
 
     def find(self, role_id: int, *args) -> Role | None:
-        self.serializer.load({'id': role_id}, partial=True)
         return self.manager.find(role_id)
 
     def get(self, **kwargs) -> dict:
-        data = self.search_serializer.load(kwargs)
-        return self.manager.get(**data)
+        return self.manager.get(**kwargs)
 
     def save(self, role_id: int, **kwargs) -> Role:
-        kwargs['id'] = role_id
-        serialized_data = self.serializer.load(kwargs)
-
-        self.manager.save(role_id, **serialized_data)
+        self.manager.save(role_id, **kwargs)
         return self.manager.find(role_id)
 
     def delete(self, role_id: int) -> Role:
-        self.serializer.load({'id': role_id}, partial=True)
         return self.manager.delete(role_id)
 
     def assign_role_to_user(self, user: User, role_id: int) -> None:
