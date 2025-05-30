6ce09ef298889172ed43553449172945c13c3c37
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-05-30 10:21:49 +0200

    test: change structure project files

---
+++ src/tests/integration/blueprints/documents/test_documents_save.py
@@ -0,0 +1,50 @@
+from urllib.parse import urlparse
+
+from flask import current_app
+
+from app.helpers.file_storage.local_storage import LocalStorage
+
+from ._base_integration_test import _BaseDocumentsIntegrationTest
+
+
+class GetDocumentsIntegrationTest(_BaseDocumentsIntegrationTest):
+    def setUp(self):
+        super().setUp()
+        self.local_storage = LocalStorage()
+
+    def test_save_document_endpoint(self):
+        pdf_file = f'{current_app.config.get("MOCKUP_DIRECTORY")}/example.pdf'
+        payload = {
+            'document': open(pdf_file, 'rb'),
+        }
+        headers = self.build_headers()
+        headers['Content-Type'] = 'multipart/form-data'
+
+        response = self.client.post(self.base_path, data=payload, headers=headers)
+        json_response = response.get_json()
+        json_data = json_response.get('data')
+        parse_url = urlparse(json_data.get('url'))
+
+        self.assertEqual(201, response.status_code)
+        self.assertEqual(self.admin_user.id, json_data.get('created_by').get('id'))
+        self.assertEqual(pdf_file, json_data.get('name'))
+        self.assertEqual('application/pdf', json_data.get('mime_type'))
+        self.assertEqual(self.local_storage.get_filesize(pdf_file), json_data.get('size'))
+        self.assertTrue(parse_url.scheme and parse_url.netloc)
+        self.assertTrue(json_data.get('created_at'))
+        self.assertEqual(json_data.get('updated_at'), json_data.get('created_at'))
+        self.assertIsNone(json_data.get('deleted_at'))
+
+    def test_check_user_roles_in_save_document_endpoint(self):
+        test_cases = [
+            (self.admin_user.email, 422),
+            (self.team_leader_user.email, 422),
+            (self.worker_user.email, 422),
+        ]
+
+        for user_email, response_status in test_cases:
+            with self.subTest(user_email=user_email):
+                response = self.client.post(self.base_path, json={}, headers=self.build_headers(user_email=user_email))
+                json_response = response.get_json()
+
+                self.assertEqual(response_status, response.status_code, json_response)
---
+++ src/tests/unit/serializers/test_role.py
@@ -0,0 +1,75 @@
+from datetime import datetime, UTC
+from unittest.mock import MagicMock
+
+from werkzeug.exceptions import BadRequest, NotFound
+
+from app.database.factories.role_factory import RoleFactory
+from app.managers import RoleManager
+from app.models import Role
+from app.repositories import RoleRepository
+from app.serializers import RoleSerializer
+from tests.base.base_test import TestBase
+
+
+class TestRoleSerializer(TestBase):
+    def setUp(self):
+        super().setUp()
+        self.role = RoleFactory(
+            name='admin',
+            description='Administrator role',
+            label='Administrator',
+            created_at=datetime.now(UTC),
+            updated_at=datetime.now(UTC),
+            deleted_at=None,
+        )
+        self.role_manager = MagicMock(spec=RoleManager)
+        self.role_manager.find.return_value = self.role
+        self.role_manager.repository = MagicMock(spec=RoleRepository)
+        self.role_manager.model = MagicMock(spec=Role)
+        self.role_manager.model.deleted_at.is_.return_value = None
+
+        self.serializer = RoleSerializer()
+        self.serializer._role_manager = self.role_manager  # pylint: disable=protected-access
+
+    def test_valid_serialization(self):
+        serialized_data = self.serializer.dump(self.role)
+
+        self.assertEqual(serialized_data['id'], self.role.id)
+        self.assertEqual(serialized_data['name'], self.role.name)
+        self.assertEqual(serialized_data['description'], self.role.description)
+        self.assertEqual(serialized_data['label'], self.role.label)
+
+    def test_validate_existing_role_id(self):
+        self.serializer.load({'id': 1}, partial=True)
+
+    def test_validate_nonexistent_role_id(self):
+        self.role_manager.find.return_value = None
+
+        with self.assertRaises(NotFound) as context:
+            self.serializer.validate_id(999)
+
+        self.assertEqual(context.exception.code, 404)
+        self.assertEqual(context.exception.description, 'Role not found')
+
+    def test_validate_deleted_role_id(self):
+        self.role.deleted_at = datetime.now(UTC)
+
+        with self.assertRaises(NotFound) as context:
+            self.serializer.validate_id(1)
+
+        self.assertEqual(context.exception.description, 'Role not found')
+
+    def test_sluglify_name(self):
+        data = {'label': 'New Role'}
+
+        result = self.serializer.load(data, partial=True)
+
+        self.assertEqual(result['name'], 'new-role')
+
+    def test_validate_existing_role_name(self):
+        self.role_manager.find_by_name.return_value = self.role
+
+        with self.assertRaises(BadRequest) as context:
+            self.serializer.load({'name': 'admin'}, partial=True)
+
+        self.assertEqual(context.exception.description, 'Role name already created')
