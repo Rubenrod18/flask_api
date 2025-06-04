b1ce0975f0ef4bec593ee29b28b88c238659f624
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-06-04 23:14:02 +0200

    chore: remove exists_attr_in_module function is not required anymore

--- src/app/utils/dynamic_imports.py
+++ src/app/utils/dynamic_imports.py
@@ -1,7 +1,4 @@
 import importlib
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 def get_attr_from_module(module: str, attr: str) -> any:
@@ -12,8 +9,7 @@ def get_attr_from_module(module: str, attr: str) -> any:
     module : str
         Module absolute path.
     attr : str
-        Module's attribute. It could be any kind of variable belongs
-        to module.
+        Module's attribute. It could be any kind of variable belongs to module.
 
     Examples
     --------
@@ -34,40 +30,3 @@ def get_attr_from_module(module: str, attr: str) -> any:
     """
     m = importlib.import_module(module)
     return getattr(m, attr)
-
-
-def exists_attr_in_module(module: str, attr: str) -> bool:
-    """Check if an attribute exists in a module.
-
-    Parameters
-    ----------
-    module : str
-        Module absolute path.
-    attr : str
-        Module's attribute. It could be any kind of variable belongs
-        to module.
-
-    Returns
-    -------
-    bool
-        True if exists, otherwise False.
-
-    Example
-    -------
-    >>> from app.utils.dynamic_imports import exists_attr_in_module
-    >>> module_path = 'app.blueprints.base'
-    >>> module_attr = 'blueprint'
-    >>> exists_attr_in_module(module_path, module_attr)
-    True
-
-    """
-    exists = False
-    try:
-        attr = get_attr_from_module(module, attr)
-        if attr:
-            exists = True
-    except (ImportError, AttributeError) as e:
-        logger.warning(e)
-        exists = False
-
-    return exists
