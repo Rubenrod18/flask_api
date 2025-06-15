d3f6e7f996747693083ccafd35ddc0b32f069d4c
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-06-15 12:49:36 +0200

    test: remove test_task_runs_without_app_context

--- src/tests/unit/celery/test_celery_app_context.py
+++ src/tests/unit/celery/test_celery_app_context.py
@@ -26,22 +26,3 @@ class TestCeleryAppContext(unittest.TestCase):
         dummy_task()
 
         self.assertEqual(result_holder['app_name'], self.app.name)
-
-    def test_task_runs_without_app_context(self):
-        def __call__(self, *args, **kwargs):
-            return self.run(*args, **kwargs)
-
-        ContextTask.__call__ = __call__
-        self.celery.Task = ContextTask
-
-        result_holder = {}
-
-        @self.celery.task(base=ContextTask)
-        def dummy_task():
-            result_holder['app_name'] = current_app.name
-            return True
-
-        with self.assertRaises(RuntimeError) as context:
-            dummy_task()
-
-        self.assertTrue(str(context.exception).find('Working outside of application context') != -1, context.exception)
