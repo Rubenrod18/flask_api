2fd54ac3e4dd08e71987a9aaf8ac8934a71eb86c
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-07-04 18:36:01 +0200

    chore(exceptions): add new general exception handler (#142)

---
+++ src/tests/integration/exceptions/test_general_exception_handler.py
@@ -0,0 +1,23 @@
+import os
+
+from app.database.factories.user_factory import UserFactory
+from tests.base.base_api_test import TestBaseApi
+
+
+class TestGeneralExceptionHandler(TestBaseApi):
+    def test_general_exception_handler(self):
+        user = UserFactory(active=True, deleted_at=None)
+
+        with self.app.container.auth_service.override(None):
+            response = self.client.post(
+                f'{self.base_path}/auth/login',
+                json={'email': user.email, 'password': os.getenv('TEST_USER_PASSWORD')},
+                exp_code=500,
+            )
+            json_response = response.get_json()
+
+        assert (
+            'The server encountered an internal error and was unable to'
+            ' complete your request. Either the server is overloaded or'
+            ' there is an error in the application.'
+        ) == json_response.get('message')
---
+++ src/tests/integration/exceptions/test_validation_error_handler.py
@@ -0,0 +1,28 @@
+import os
+from unittest.mock import MagicMock
+
+from marshmallow import ValidationError
+
+from app.database.factories.user_factory import UserFactory
+from app.services import AuthService
+from tests.base.base_api_test import TestBaseApi
+
+
+class TestValidationErrorHandler(TestBaseApi):
+    def test_validation_error_handler(self):
+        user = UserFactory(active=True, deleted_at=None)
+
+        mock_auth_service = MagicMock(spec=AuthService)
+        sample_error_msg = 'sample error message'
+        mock_auth_service.login_user.side_effect = ValidationError(field_name='email', message=sample_error_msg)
+
+        with self.app.container.auth_service.override(mock_auth_service):
+            self.app.debug = False
+            response = self.client.post(
+                f'{self.base_path}/auth/login',
+                json={'email': user.email, 'password': os.getenv('TEST_USER_PASSWORD')},
+                exp_code=422,
+            )
+            json_response = response.get_json()
+
+        assert {'message': [sample_error_msg]} == json_response
