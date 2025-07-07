481d15d39fea64732f656ce2673424748a610841
Rubén Rodríguez Ramírez <rubenrod.018@gmail.com>
2025-07-07 18:26:16 +0200

    style(pylint): apply rules

--- src/config.py
+++ src/config.py
@@ -20,6 +20,11 @@ def _str_to_bool(env_value: str, default_env_value: str) -> bool:
     return env_value.lower() == 'true'
 
 
+def _str_to_int(env_value: str, default_env_value: str) -> int:
+    env_value = env_value or default_env_value
+    return int(env_value)
+
+
 class Meta(type):
     """Metaclass for updating Config options."""
 
@@ -104,7 +109,7 @@ class Config(metaclass=Meta):
 
     # Mr Developer
     HOME = os.getenv('HOME')
-    LOGGING_LEVEL = os.getenv('LOGGING_LEVEL', logging.INFO)
+    LOGGING_LEVEL = _str_to_int(os.getenv('LOGGING_LEVEL', logging.INFO))
 
     TEST_USER_EMAIL = os.getenv('TEST_USER_EMAIL')
     TEST_USER_PASSWORD = os.getenv('TEST_USER_PASSWORD')
